6d446b86bde117d9c86f9ecad54128ca4824cbd3
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-03-21 19:44:42 -0400

    exported and imported modules for each respective class and test

---
+++ __tests__/employee.test.js
@@ -0,0 +1,15 @@
+const Employee = require('../lib/employee');  //importing the class file
+
+// template of test:
+describe("Toy", () => {
+    describe("init", () => {
+      it('should…', () => {
+  
+      });
+    });
+    describe("purchase", () => {
+      it('should…', () => {
+  
+      });
+    });
+  });
