e0ca4d0be1472cb6f1dfbd2cdea34c19247b47a1
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-03-21 19:22:21 -0400

    reinstalled the correct jest package and renamed the tests directory by adding the underscores. Also created class files

---
+++ index.js
@@ -0,0 +1,10 @@
+// create all 4 classes
+// export & import files accordingly
+// psudeo code:
+    // employee
+    // engineer
+    // intern
+    // manager
+
+// create html
+// using bootstrap style the cards
