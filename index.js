611da3452e4d719779b9a1a700363aacc0a2471f
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-03-22 21:34:55 -0400

    1st set of general questions updated and app ran in console

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-// create all 4 classes --DONE
-// export & import files accordingly --DONE
-// psudeo code tests:
-    // employee
-    // engineer
-    // intern
-    // manager
-
+// create questions 
 // create html
 // using bootstrap style the cards
-
 
     // importing the package/module: inquirer
 const inquirer = require('inquirer');
@@ -17,39 +9,43 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 //importing the generate html template file
-const HtmlBody = require('./src/generateHtmlTemplate');  
+// const HtmlBody = require('./src/generateHtmlTemplate');  
 
+// const init = () => {  //we dont want to write any code in global scope or easy to get lost
 
-const questions = [
+// };
+// init();
+
+const generalQuestions = [
     {
         type: 'input',
-        message: 'What is your team manager\'s name?',
-        name: 'Manager Name' // The name property is a must since it is used in the storing of answers
+        message: 'What is your/employee\'s name?',
+        name: 'Name' // The name property is a must since it is used in the storing of answers
     },
     {
         type: 'input',
-        message: 'What is your team manager\'s employee ID?',
-        name: 'Manager employee ID' // The name property is a must since it is used in the storing of answers
+        message: 'What is your/employee\'s ID?',
+        name: 'ID' 
     },
     {
         type: 'input',
-        message: 'What is your team manager\'s email address?',
-        name: 'Manager employee ID' // The name property is a must since it is used in the storing of answers
+        message: 'What is your/employee\'s email address?',
+        name: 'Email' 
     },
     {
         type: 'input',
         message: 'What is your team manager\'s office number?',
-        name: 'Manager employee ID' // The name property is a must since it is used in the storing of answers
+        name: 'Manager employee ID' 
     },
     {
         type: 'rawlist',
         message: 'What is your team manager\'s office number?',
-        name: 'Manager employee ID' // The name property is a must since it is used in the storing of answers
+        name: 'Manager employee ID' 
     },
-
-
-    },
+    
+    //     },
 ];
+// console.log(questions);
 
 // function writeToFile(fileName, data) { //ppparameters
 //     // err catch taken from activity 13 line 15-16
@@ -61,9 +57,12 @@ const questions = [
 // });
 // }
    
-// // TODO: Create a function to initialize app
-// function init() {
-//    inquirer.prompt(questions) //calling/passing the questions object from above--.using the prompt method from inquirer (object) module that I imported at the top 
+// function to initialize app
+function init() {
+   inquirer.prompt(generalQuestions)
+
+}
+  //calling/passing the questions object from above--.using the prompt method from inquirer (object) module that I imported at the top 
 //    .then(function (responses){          //after ?'s are asked .then returns promise and gives data (aka the answers) and then
 //        writeToFile('./dist/README.md', generateMarkdown(responses));  //-calling writeToFile function and passing arguments: 1. fileName is now path of where to create the file 2. data is now pulling template literal from generateMarkdown file which contains answers to questions
 //        // console.log(responses.title)
@@ -71,5 +70,5 @@ const questions = [
 //    })};
 // // )}
 
-// // Function call to initialize app
-// init();
+// Function call to initialize app
+init();
